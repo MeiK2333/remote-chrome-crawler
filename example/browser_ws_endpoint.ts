615387f7b0e5599662157de96e07b4425de58658
MeiK <meik2333@gmail.com>
2019-11-21 16:00:06 +0800

    Update

--- example/browser_ws_endpoint.ts
+++ example/browser_ws_endpoint.ts
@@ -1,26 +1,18 @@
-/**
- * 无法通过验证，通过验证的示例请参考 browser_ws_endpoint.ts
- */
 import { Queue, CrawlerQueue } from "../src/queue"
 import { Task } from "../src/task"
-import { puppeteerStealth } from "../src/puppeteer-stealth"
 import puppeteer from 'puppeteer'
 
 (async () => {
     Queue.createBrowser = async function createBrowser(queue: CrawlerQueue) {
-        const browser = puppeteer.launch({
-            headless: false
+        const browser = puppeteer.connect({
+            // /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')
+            browserWSEndpoint: 'ws://127.0.0.1:9222/devtools/browser/6598b562-365e-4c7d-9fc8-9568af528761',
         })
         return browser
     }
     Queue.createPage = async function (task: Task) {
         const content = await task.queue.browser.createIncognitoBrowserContext()
         const page = await content.newPage()
-        // await page.setViewport({
-        //     width: 1920,
-        //     height: 1080
-        // })
-        await puppeteerStealth(page)
         await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36")
         return page
     }
@@ -33,6 +25,7 @@ import puppeteer from 'puppeteer'
             await content.close()
         }
     }
+    // Queue.closeBrowser = async function () { }
     await Queue.add(new Task('https://fingerprintjs.com/demo', { callback: main }))
     await Queue.run()
 })()
